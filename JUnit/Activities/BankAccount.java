3a8b8f0f95f49373cd048d1b78f0ab7a81b43706
ManojIBM89 <MANOJKUMAR.K.G@ibm.com>
2025-02-19 14:23:12 +0530

    Add files via upload

---
+++ JUnit/Activities/BankAccount.java
@@ -0,0 +1,21 @@
+package Activities;
+
+public class BankAccount {
+	private Integer balance;
+
+	// create a constructor
+	public BankAccount(Integer initialBalance) {
+		balance = initialBalance;
+	}
+
+	// add method to calculate
+	// balance amount after withdrawal
+	public Integer withdraw(Integer amount) {
+		if (balance < amount) {
+			throw new NotEnoughFundsException(amount, balance);
+		}
+		balance -= amount;
+		return balance;
+	}
+
+}
